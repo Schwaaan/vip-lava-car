d917741299125fde77670738f18ce2cce5cd6d3d
Schwaaan <jair.schwan9@gmail.com>
2021-04-21 16:15:39 -0300

    tentando resolver cors

--- be/server.js
+++ be/server.js
@@ -3,12 +3,13 @@ const cors = require('cors');
 
 const app = express();
 const port = 6000;
+const hostname = "0.0.0.0";
 const usuariosRouter = require('./routes/usuarios');
 
 app.use(cors());
 app.use(express.json());
 app.use('/usuarios', usuariosRouter);
 
-app.listen(port, () => {
+app.listen(port,hostname, () => {
 console.log(`O servidor está rodando na porta: ${port}`);
 });
